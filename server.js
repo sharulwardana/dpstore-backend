a0646509cb9c7dc1e5fdb1d125dc46ccc92fc34e
sharulwardana <sharulwrdn10@gmail.com>
2025-07-30 17:08:58 +0700

    Update database connection to use DATABASE_URL

--- server.js
+++ server.js
@@ -66,11 +66,10 @@ async function sendEmailNotification(to, subject, htmlContent) {
 
 // --- Konfigurasi Koneksi Database ---
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 // --- Middleware Global ---
