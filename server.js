563b81157ecba8c69b932fd6750b07b4d90c4b70
sharulwardana <sharulwrdn10@gmail.com>
2025-08-11 10:33:46 +0700

    add healthcheck

--- server.js
+++ server.js
@@ -73,7 +73,8 @@ async function startServer() {
         'https://zingy-zabaione-a27ed6.netlify.app',
         'http://localhost:5173',
         'http://127.0.0.1:5500',
-        'https://dpstore-backend-production.up.railway.app'
+        'https://dpstore-backend-production.up.railway.app',
+        'https://healthcheck.railway.app'
     ];
     const corsOptions = {
         origin: (origin, callback) => {
